f0db5ddd50a3be5e304b11f166ed1da790333ffd
Vincent Caracciolo <vincentncaracciolo@gmail.com>
2019-10-29 18:29:56 -0400

    everything working, connection.end error taken care of

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -47,35 +47,57 @@ function shopping() {
             message: "Please enter the item Id of the product you wish to purchase!"
         })
         .then(function (answer1) {
-                var selection = answer1.productToBuy;
-                connection.query("SELECT * FROM products WHERE Id=?", selection, function (
-                            err,
-                            res
-                        ) {
-                            if (err) throw err;
-                            if (res.length === 0) {
+            var selection = answer1.productToBuy;
+            connection.query("SELECT * FROM products WHERE item_id=?", selection, function (
+                err,
+                res
+            ) {
+                if (err) throw err;
+                if (res.length === 0) {
+                    console.log(
+                        "That product doesn't exist, please enter an item Id from the list above."
+                    );
+                    shopping();
+                } else {
+                    inquirer
+                        .prompt({
+                            name: "quantity",
+                            type: "input",
+                            message: "How many items would you like to purchase?"
+                        })
+                        .then(function (answer2) {
+                            var quantity = answer2.quantity;
+                            if (quantity > res[0].stock_quantity) {
                                 console.log(
-                                    "That product doesn't exist, please enter an item Id from the list above."
+                                    "Our Apologies we only have " +
+                                    res[0].stock_quantity +
+                                    " items of the product selected"
                                 );
                                 shopping();
                             } else {
-                                inquirer
-                                    .prompt({
-                                        name: "quantity",
-                                        type: "input",
-                                        message: "How many items woul you like to purchase?"
-                                    })
-                                    .then(function (answer2) {
-                                            var quantity = answer2.quantity;
-                                            if (quantity > res[0].stock_quantity) {
-                                                console.log(
-                                                    "Our Apologies we only have " +
-                                                    res[0].stock_quantity +
-                                                    " items of the product selected"
-                                                );
-                                                shopping();
-                                            } else {
-                                                console.log("");
-                                                console.log(res[0].product_name + " purchased");
-                                                console.log(quantity + " qty @ $" + res[0].price);
-                                            
+                                console.log("");
+                                console.log(res[0].product_name + " purchased");
+                                console.log(quantity + " qty @ $" + res[0].price);
+                                var newQuantity = res[0].stock_quantity - quantity;
+                                connection.query(
+                                    "UPDATE products SET stock_quantity = " +
+                                    newQuantity +
+                                    " WHERE item_id = " +
+                                    res[0].item_id,
+                                    function (err) {
+                                        if (err) throw err;
+                                        console.log("");
+                                        console.log("Your Order has been Processed");
+                                        console.log("Thank you for Shopping with us...!");
+                                        console.log("");
+                        
+                                    }
+                                );
+                            }
+                        });
+                }
+            });
+        });
+};
+
+display();
